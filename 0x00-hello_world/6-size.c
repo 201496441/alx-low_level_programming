48b409b6c7c2fd1366198cfca113be1b820d3150
Thabo <magorothabo1@gmail.com>
2022-12-08 04:08:32 -0800

    4-puts.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-printf("Size of a char: %lu bytes(s)\n",sizeof(char));
-printf("Size of a int: %lu bytes(s)\n",sizeof(int));
-printf("Size of a long int: %lu bytes(s)\n",sizeof(long int));
-printf("Size of a long long: %lu bytes(s)\n",sizeof(long long int));
-printf("Size of a float: %lu bytes(s)\n",sizeof(float));
+printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+printf("Size of a int: %lu bytes(s)\n", sizeof(int));
+printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+printf("Size of a long long: %lu bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %lu bytes(s)\n", sizeof(float));
 return (0);
 }
