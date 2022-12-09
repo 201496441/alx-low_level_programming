12c6a450e857c1ec0d5b06586913a8712e5b7635
Thabo <magorothabo1@gmail.com>
2022-12-09 06:36:38 -0800

    Hello world

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{char c;
+c='a';
+while (c <= 'z'){
+if (( c != 'q' && c != 'e') && c <= 'z')
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
