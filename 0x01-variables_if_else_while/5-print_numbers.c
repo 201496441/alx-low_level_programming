beb003a747effb41493ffc8ab6b9caa5607bc68d
Thabo <magorothabo1@gmail.com>
2022-12-09 06:28:40 -0800

    Hello world

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+ #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+	printf("%d", a);
+putchar('\n');
+return (0);
+}
