6a24174016fa984f1e67a657730bdd85ca5919f4
Thabo <magorothabo1@gmail.com>
2022-12-09 07:55:35 -0800

    fix number 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,14 +12,15 @@
 
 int main(void)
 {
-for (int a = 0; a < 100; a++){
+for (int a = 0; a < 100; a++)
+{
 putchar(a + '0');
-if (a < 99){
+if (a < 99)
+{
 putchar(',');
 putchar(' ');
 }
 }
 putchar('\n');
-
 return (0);
 }
