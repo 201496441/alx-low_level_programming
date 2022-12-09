a02dc93999364756beea3436c56dc688f9b6b35b
Thabo <magorothabo1@gmail.com>
2022-12-09 07:42:47 -0800

    Hello world

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+for (int a = 0; a < 100; a++){
+putchar(a + '0');
+if (a < 99){
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
