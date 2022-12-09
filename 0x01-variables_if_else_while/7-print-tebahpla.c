94e81fad0e5a758814f542fb200132182dc5882e
Thabo <magorothabo1@gmail.com>
2022-12-09 07:04:50 -0800

    Hello world

---
+++ 0x01-variables_if_else_while/7-print-tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char a;
+a = 'a';
+while (a <= 'z')
+{
+putchar(a);
+a--;
+}
+putchar('\n');
+
+return (0);
+}
