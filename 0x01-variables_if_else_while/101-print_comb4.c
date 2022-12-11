03e26d60ec4ee4d34a2fde31b8156913688836ca
Thabo <magorothabo1@gmail.com>
2022-12-11 03:44:11 -0800

    fix 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - returns a combination of number
- * Return: return 0 if successful
+ * main - Prints 3 combination of numbers
+ *
+ * Return: Always (Success)
  */
-
 int main(void)
 {
-int c;
-int d;
-int e = 0;
-while (e < 10)
-{
-d = 0;
-while (d < 10)
-c = 0;
-while (c < 10)
-{
-if (c != d && d != e && e < d && d < c)
-{
-putchar('o' + e);
-putchar('o' + d);
-putchar('o' + c);
-if (c + d + e != 7 + 8 + 9)
-{
-putchar(',');
-putchar(' ');
-}
-c++;
-}
-d++;
-}
-e++;
-}
-putchar('\n');
-return (0);
+	int c, i, k;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
