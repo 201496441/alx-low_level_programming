4a79026cb1ec285a17cabcc19bbfb74074a6249b
Thabo <magorothabo1@gmail.com>
2022-12-13 03:37:43 -0800

    number 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Return: 0
+ */
+void print_alphabet(void) /** print function print_alphabet Entry point */
+{
+char a = 'a';
+for (int i =0; i <= 10; i++)
+{
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+}
+_putchari('\n');
+
+}
+
+
