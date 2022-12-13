4c734fbee0b56c0bd1fa92165bd06f2e509a8f0a
Thabo <magorothabo1@gmail.com>
2022-12-13 03:13:42 -0800

    number 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+}
