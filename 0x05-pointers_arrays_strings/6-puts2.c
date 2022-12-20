89dca2db2755f235650d40610fc655f4f354e071
Thabo <magorothabo1@gmail.com>
2022-12-20 01:37:16 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,15 @@
 void puts2(char *str)
 {
 	int i;
-i = 0;
-while (str[i] != '\0')
-{
-if (i % 2 == 0)
-{
-_putchar(str[i]);
-i++;
-}
-}
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		 i++;
+	}
+
+	_putchar('\n');
 }
