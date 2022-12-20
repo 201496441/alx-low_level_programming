d10ef08a36fe58da4891d8a67c7b2c309daa8dd1
Thabo <magorothabo1@gmail.com>
2022-12-19 22:22:05 -0800

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,6 @@
+#include <stdio>
+#include "main.h"
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+void reset_to_98(int *n);
+#endif
