e4bbd1df5cda934180c22f0a469314294030c41f
Thabo <magorothabo1@gmail.com>
2022-12-20 01:30:28 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+* puts2 - function that prints a string, followed by a new line, to stdout
+* @str: pointer value
+* Return: Always 0 (Success)
+*/
 void puts2(char *str)
 {
 	int i;
