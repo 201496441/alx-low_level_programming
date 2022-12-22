02774f768e4b020a3cd42db0b347f9d98d25ea01
Thabo <magorothabo1@gmail.com>
2022-12-21 23:23:14 -0800

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,11 +16,11 @@ char *_strcat(char *dest, char *src)
 	{
 		length++;
 	}
-	for (j = 0; src[j] <= '\0'; j++; length++)
+	for (j = 0; src[j] != '\0'; j++, length++)
 	{
 		dest[length] = src[j];
 	}
+
 	dest[length] = '\0';
 	return (dest);
-
 }
