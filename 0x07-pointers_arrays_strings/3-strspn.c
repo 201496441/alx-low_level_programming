21a3b73c77081099c700a0ac1506f4de4bed152a
201496441 <magorothabo1@gmail.com>
2023-01-03 17:18:35 +0200

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _strspn - return length of string that matches values 
+* _strspn - return length of string that matches values consistently
 * @s: string to search
 * @accept: target matches
 * Return: number of bytes consecutively matched
@@ -33,4 +33,3 @@ unsigned int _strspn(char *s, char *accept)
 	return (matches);  
 
 }
-
