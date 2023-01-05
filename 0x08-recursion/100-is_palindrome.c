e7ce4afe2e59d037289b194d9bc42ff04fd8d1f2
201496441 <magorothabo1@gmail.com>
2023-01-05 11:26:12 +0200

    task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * palindromeChecker - checks the string
+ * @str: the string to be checked
+ * @len: the length of the string
+ * @i: the incrementor, starts at 0
+ * Return: 1 if it's a palindrome, 0 if it's not
+ */
+int palindromeChecker(char *str, int len, int i)
+{
+	if (i < len && str[i] == str[len])
+		return (palindromeChecker(str, len - 1, i + 1));
+	if (str[i] != str[len])
+		return (0);
+	return (1);
+}
+/**
+ * is_palindrome - checks to see if a string is a palindrome
+ * @s: the string to check
+ * Return: 1 if it's a palindrome, 2 if it's not
+ */
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int length = _strlen_recursion(s) - 1;
+	return (palindromeChecker(s, length, i));
+}
