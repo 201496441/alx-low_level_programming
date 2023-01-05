047741d88e2658384468e5db44b028609099bac2
201496441 <magorothabo1@gmail.com>
2023-01-05 09:25:57 +0200

    0x08. C - Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - prints a string in reverse
+ *@s: pointer block of memory to fill
+ *Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+_print_rev_recursion(s + 1);
+_putchar(s);
+}
