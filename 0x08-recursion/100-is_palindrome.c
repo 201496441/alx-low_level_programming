be3202b9a6385259c96914a98904759dfa447648
201496441 <magorothabo1@gmail.com>
2023-01-05 11:28:39 +0200

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,17 @@
 #include "main.h"
 /**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to check the length of
+ * Return: an integer that displays the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+
+	return (0);
+}
+/**
  * palindromeChecker - checks the string
  * @str: the string to be checked
  * @len: the length of the string
