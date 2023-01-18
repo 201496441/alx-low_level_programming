42db87276262d111011eef7a386dccdf89d82bb4
201496441 <magorothabo1@gmail.com>
2023-01-18 09:38:08 +0200

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (!array || !action)
+	{
+		return;
+	}
+	unsigned int i;
+	for (i = 0;i <= size; i++)
+	{
+		action(array[i]);
+	}
+	
+}
