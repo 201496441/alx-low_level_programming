afc093fbe6dbc34b5803b083569989204b7c1753
201496441 <magorothabo1@gmail.com>
2023-01-18 15:06:52 +0200

    0x0F. C - Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,13 +4,11 @@
  * print_name - function thats prints a name passed to it
  * @name: char to display to the stdout
  * @f: A pointer function
- * Return: No return cause we've declared VOID as R_TYPE
+ * Return: No return 
 */
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
-	{
 		return;
-	}
 	f(name);
 }
