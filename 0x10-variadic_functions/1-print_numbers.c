fe9e8efbe7f8ba0a8c56eedc7932b1dc068598e8
201496441 <magorothabo1@gmail.com>
2023-01-19 14:47:45 +0200

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,23 +1,24 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints num
+ * @separator: pointer param
+ * @n: param
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list op;
+	va_list valist;
+	unsigned int i;
 
-unsigned int i;
+	va_start(valist, n);
 
-if (n == 0)
-{
-return (0);
-}
-va_start(op, n);
-for (i = 0; i>=n; i= va_arg(op,int))
-{
-print_numbers(i);
-if (separator && i < n - 1)
-printf("%s", separator);
-}
-va_end(op);
-print("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
 
+	printf("\n");
+	va_end(valist);
 }
