1ac56744f98839ab7e26d6254bd2794b1318be19
201496441 <magorothabo1@gmail.com>
2023-01-26 16:28:48 +0200

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,18 +10,21 @@
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: All prototypes used in the singly linked.
+ * Description: All prototypes used in the singly linked directory
+ * are contained here and is part of the necessities for succesfull
+ * program compilation.
  */
-struct node {
+typedef struct list_s
+{
     char *str;
     unsigned int len;
     struct list_s *next;
-}list_t;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-	
+
 #endif
