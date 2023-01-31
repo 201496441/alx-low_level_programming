88fd1a43a3fd3d1a3a9257c3bfcd1c65de53e927
Thabo <magorothabo1@gmail.com>
2023-01-31 00:47:17 -0800

    fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,11 +5,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t elements =0;
+	size_t elements = 0;
+
 	while (h)
 	{
 		elements++;
-		h=h->next;
+		h = h->next;
 	}
 	return (elements);
 }
