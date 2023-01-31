8ec2e4dc4baca3b22c45270676f457f26da4baf6
Thabo <magorothabo1@gmail.com>
2023-01-31 07:31:42 -0800

    task 4 fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,6 +7,7 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
 	if (*head == NULL)
 	{
 	return;
@@ -18,5 +19,5 @@ void free_listint2(listint_t **head)
 		*head = temp;
 	
 	}
-	*head =NULL;
+	*head = NULL;
 }
