5a0eea186b8811aaf28159ce8aa61d050dd1fa0d
Thabo <magorothabo1@gmail.com>
2023-01-31 08:50:18 -0800

    task 4 fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,16 +8,16 @@
  */
 int pop_listint(listint_t **head)
 {
-  listint_t *temp;
-  int head_data;
+listint_t *temp;
+int head_data;
 
-  if (!*head ||!head)
-  {
-	  return (0);
-  }
-  head_data = (*head)->n;
-  temp = *head;
-  *head = (*head)->next;
-  free(temp);
-  return (head_data);
+if (!*head || !head)
+{
+return (0);
+}
+head_data = (*head)->n;
+temp = *head;
+*head = (*head)->next;
+free(temp);
+return (head_data);
 }
