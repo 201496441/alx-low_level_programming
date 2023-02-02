07841586af98947d9f6db1f7bb72faf33d3cbc7f
201496441 <magorothabo1@gmail.com>
2023-02-02 20:27:31 +0200

    fix

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * get_endianness - checks if a machine is little or big endian
- * Return: 0 for big, 1 for little
- */
+* get_endianness - checks if a machine is little or big endian
+* Return: 0 for big, 1 for little
+*/
 int get_endianness(void)
 {
-    unsigned int i = 1;
-    char *c = (char *)&i;
+unsigned int i = 1;
+char *c = (char *)&i;
 
 return (*c);
 }
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at an index in a decimal number
- * @n: number to search
- * @index: index of the bit
- *
- * Return: value of the bit
- */
+* get_bit - returns the value of a bit at an index in a decimal number
+* @n: number to search
+* @index: index of the bit
+*
+* Return: value of the bit
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
-    int bit_val;
+int bit_val;
 
-    if (index > 63)
-        return (-1);
+if (index > 63)
+return (-1);
 
-    bit_val = (n >> index) & 1;
+bit_val = (n >> index) & 1;
 
-    return (bit_val);
+return (bit_val);
 }
