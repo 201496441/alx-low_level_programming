e5fc28f0af15ae7919f374f0d9dd60b01abc5aee
Thabo <magorothabo1@gmail.com>
2023-03-16 06:07:20 -0700

    fix betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,24 +9,21 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
-	
+
 	count = 0;
 
-	if(h == NULL)
+	if (h == NULL)
 	{
 		return (count);
 	}
-	
 	while (h->prev != NULL)
 	{
 		h = h->prev;
 	}
-	
 	while (h != NULL)
 	{
 		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
